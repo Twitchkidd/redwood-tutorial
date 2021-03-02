d3b208f45a5540496457864ad79e2dcdfa135cce
Gareth Field <field.gareth@gmail.com>
2021-03-02 11:10:00 -0500

    up to auth, whoops

--- web/src/components/BlogPost/BlogPost.js
+++ web/src/components/BlogPost/BlogPost.js
@@ -1,9 +1,10 @@
 import { Link, routes } from '@redwoodjs/router';
+import CommentsCell from 'src/components/CommentsCell';
+import CommentForm from 'src/components/CommentForm';
 
 const truncate = (text, length) => {
   return text.substring(0, length) + '...';
 };
-
 const BlogPost = ({ post, summary = false }) => {
   return (
     <article>
@@ -15,6 +16,14 @@ const BlogPost = ({ post, summary = false }) => {
       <div className="mt-2 text-gray-900 font-light">
         {summary ? truncate(post.body, 100) : post.body}
       </div>
+      {!summary && (
+        <div className="mt-16">
+          <CommentForm postId={post.id} />
+          <div className="mt-24">
+            <CommentsCell postId={post.id} />
+          </div>
+        </div>
+      )}
     </article>
   );
 };
--- web/src/components/BlogPost/BlogPost.test.js
+++ web/src/components/BlogPost/BlogPost.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@redwoodjs/testing';
+import { render, screen, waitFor } from '@redwoodjs/testing';
 import BlogPost from './BlogPost';
+import { standard } from 'src/components/CommentsCell/CommentsCell.mock';
 
 const POST = {
   id: 1,
@@ -17,6 +18,15 @@ describe('BlogPost', () => {
   });
   it('renders the full blog post with explicit no summary', () => {
     render(<BlogPost post={POST} summary={false} />);
+    expect(screen.getByText(POST.title)).toBeInTheDocument();
+    expect(screen.getByText(POST.body)).toBeInTheDocument();
+  });
+  it('renders comments when displaying a full blog post', async () => {
+    const comment = standard().comments[0];
+    render(<BlogPost post={POST} />);
+    await waitFor(() =>
+      expect(screen.getByText(comment.body)).toBeInTheDocument()
+    );
   });
   it('renders a summary of a blog post', () => {
     render(<BlogPost post={POST} summary={true} />);
@@ -26,5 +36,12 @@ describe('BlogPost', () => {
         'Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Str...'
       )
     ).toBeInTheDocument();
+  });
+  it('does not render comments when displaying a summary', async () => {
+    const comment = standard().comments[0];
+    render(<BlogPost post={POST} summary={true} />);
+    await waitFor(() =>
+      expect(screen.queryByText(comment.body)).not.toBeInTheDocument()
+    );
   });
 });
